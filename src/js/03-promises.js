4f1a3f3d04ae389f6c05976eb0c4eabe4367be7e
andriideikalo <wlada801@gmail.com>
2022-11-18 10:21:01 +0200

    ,

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,17 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+        // Fulfill
+    } else {
+        // Reject
+    }
 }
+createPromise(2, 1500)
+    .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
