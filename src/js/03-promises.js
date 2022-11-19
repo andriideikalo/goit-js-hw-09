11a481af9652d80ef0521cf635be261adef096ce
andriideikalo <wlada801@gmail.com>
2022-11-19 23:26:57 +0200

    add task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,43 @@
-import Notiflix from 'notiflix';
-
+import Notiflix, { Notify } from 'notiflix';
+// __________________________________________________________________________________________________________
 function createPromise(position, delay) {
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-        // Fulfill
-    } else {
-        // Reject
-    }
+    setTimeout(() => {
+        if (shouldResolve) {
+            Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+        } else {
+            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+        }
+    }, delay)
 }
-createPromise(2, 1500)
-    .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
+const refs = {
+        form: document.querySelector('.form'),
+
+    }
+    // console.dir(refs.form)
+refs.form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    let delay = +evt.currentTarget.delay.value;
+    const step = +evt.currentTarget.step.value;
+    const amount = +evt.currentTarget.amount.value;
+
+
+    // console.log(Number(delay))
+    // console.log(Number(step))
+    // console.log(Number(delay) + Number(step))
+    // console.log(createPromise)
+
+    for (let i = 1; i < amount; i += 1) {
+        delay += step
+        createPromise(i, delay)
+    }
+
+    function onSucces({ position, delay }) {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }
+
+    function onError({ position, delay }) {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+
+})
